21473032441d303e7fdf64df09af2f544917d20c
noortaz <noortazc@gmail.com>
2019-08-28 13:57:40 -0400

    added comments and organised codes

--- server/index.js
+++ server/index.js
@@ -3,24 +3,23 @@ const cors = require('cors');
 const app = express();
 const uuid = require('uuid/v1');
 
+
+// require packages for sending text via twilio
 const twilio = require('twilio')
 const accountSid = 'AC17364c31e803e7f143e9b7a08fef27b2';
 const authToken = '021f61d3069bbff66ce792f71bc84380';
 const client = new twilio(accountSid, authToken);
 
-
+// get my json files containing data
 const projectData =  require('./data/projectData.json');
-const taskData = require('./data/taskData.json');
 let combinedData = require('./data/combinedData.json');
-
-
-const firebase = require("firebase/app");
-const firestore = require("firebase/firestore");
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+
+// set up twilio
 app.get('/sendSMS', (req, res) => {
   const {recipient, textmessages} = req.query;
 
@@ -35,23 +34,7 @@ app.get('/sendSMS', (req, res) => {
 })
 
 
-// // My web app's Firebase configuration
-// const firebaseConfig = {
-//     apiKey: "AIzaSyDZcCHoB-R5u0mvAXteZMX0f0PRs934Hpg",
-//     authDomain: "hometeam-84ccd.firebaseapp.com",
-//     databaseURL: "https://hometeam-84ccd.firebaseio.com",
-//     projectId: "hometeam-84ccd",
-//     storageBucket: "hometeam-84ccd.appspot.com",
-//     messagingSenderId: "107019629244",
-//     appId: "1:107019629244:web:2ce47cd9ec3a3ab2"
-// };
-// // Initializing Firebase
-// firebase.initializeApp(firebaseConfig);
-
-app.get('/projectData', (req, res) => {
-    res.json(projectData);
-})
-
+//get and post data for dragdrop page
 app.get('/taskData', (req, res) => {
     res.json(combinedData);
 })
@@ -69,8 +52,13 @@ app.post('/taskData', (req, res) => {
     res.json(combinedData);
 })
 
+
+// get and post the data for my project page
+app.get('/projectData', (req, res) => {
+  res.json(projectData);
+})
+
 app.post('/projectData', (req, res) => {
-    
     const project = req.body;
     const newProject= { ...project, projectId: `${uuid().substr(0, 12)}` }
     projectData.push(newProject);
@@ -78,6 +66,28 @@ app.post('/projectData', (req, res) => {
 })
 
 
+
+// Listen to the port
 app.listen(process.env.PORT || 8080, () => {
     console.log('Server is running...')
 })
+
+
+
+/***************Firebase************/
+
+// const firebase = require("firebase/app");
+// const firestore = require("firebase/firestore");
+
+// // My web app's Firebase configuration
+// const firebaseConfig = {
+//     apiKey: "AIzaSyDZcCHoB-R5u0mvAXteZMX0f0PRs934Hpg",
+//     authDomain: "hometeam-84ccd.firebaseapp.com",
+//     databaseURL: "https://hometeam-84ccd.firebaseio.com",
+//     projectId: "hometeam-84ccd",
+//     storageBucket: "hometeam-84ccd.appspot.com",
+//     messagingSenderId: "107019629244",
+//     appId: "1:107019629244:web:2ce47cd9ec3a3ab2"
+// };
+// // Initializing Firebase
+// firebase.initializeApp(firebaseConfig);
